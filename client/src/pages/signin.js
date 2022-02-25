2e63afc618b8eb9840748ba02c387cf5adcf37e8
Jason Offenbaker <JasonOffenbaker@gmail.com>
2022-02-25 15:09:36 -0700

    Added signup/login form

--- client/src/pages/signin.js
+++ client/src/pages/signin.js
@@ -3,38 +3,66 @@ import React from "react";
 const signIn = () => {
 
     return (
-        <div class="">
-            <h2>Sign up</h2>
-            <div class="uk-flex uk-flex-row uk-flex-center">
+        <div class="uk-flex uk-flex-center ">
+            <div class="uk-text-left uk-card-body uk-tile uk-width-1-4@m">
+                <h2 class="uk-flex uk-flex-center">Login</h2>
                 <form>
+
                     <div class="uk-margin">
                         <div class="uk-inline">
-                            <span class="uk-form-icon" uk-icon="icon: user"></span>
-                            <input class="uk-input" type="text" />
+                            <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: user"></span>
+                            <input class="uk-input" type="text" placeholder="Username or E-mail"/>
                         </div>
                     </div>
+
                     <div class="uk-margin">
                         <div class="uk-inline">
                             <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
-                            <input class="uk-input" type="text" />
+                            <input class="uk-input" type="text" placeholder="Password"/>
                         </div>
                     </div>
+
                 </form>
-                <hr class="uk-divider-vertical"></hr>
-                <h2>Login</h2>
+            </div>
+            <div class="uk-text-right uk-card-body uk-tile uk-width-1-4@m">
+                <h2 class="uk-flex uk-flex-center">Sign Up</h2>
                 <form>
+
                     <div class="uk-margin">
                         <div class="uk-inline">
                             <span class="uk-form-icon" uk-icon="icon: user"></span>
-                            <input class="uk-input" type="text" />
+                            <input class="uk-input" type="text"  placeholder="Username"/>
                         </div>
                     </div>
+
                     <div class="uk-margin">
                         <div class="uk-inline">
-                            <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
-                            <input class="uk-input" type="text" />
+                            <span class="uk-form-icon" uk-icon="icon: user"></span>
+                            <input class="uk-input" type="text"  placeholder="E-mail"/>
                         </div>
                     </div>
+
+                    <div class="uk-margin">
+                        <div class="uk-inline">
+                            <span class="uk-form-icon" uk-icon="icon: user"></span>
+                            <input class="uk-input" type="text"  placeholder="Confirm E-mail"/>
+                        </div>
+                    </div>
+
+                    <div class="uk-margin">
+                        <div class="uk-inline">
+                            <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                            <input class="uk-input" type="text" placeholder="Password"/>
+                        </div>
+                    </div>
+
+                    <div class="uk-margin">
+                        <div class="uk-inline">
+                            <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                            <input class="uk-input" type="text" placeholder=" Confirm Password"/>
+                        </div>
+                    </div>
+
                 </form>
             </div>
         </div>
