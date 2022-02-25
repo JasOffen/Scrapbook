ff4e25d2bf8e001a571e3adda3be9c4d834082be
Jason Offenbaker <JasonOffenbaker@gmail.com>
2022-02-25 02:57:46 -0700

    basic navbar added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,38 +3,36 @@ import React from "react";
 const Navbar = () => {
 
     return (
-        <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar="">
-    <div class="uk-navbar-left">
+        <nav class="uk-navbar-container uk-margin uk-navbar-transparent" uk-navbar=''>
 
-        <ul class="uk-navbar-nav">
-            <li class="uk-active"><a href="#">Active</a></li>
-            <li>
-                <a href="#">Parent</a>
-            </li>
-            <li><a href="#">Item</a></li>
-        </ul>
+            <div class="nav-overlay uk-navbar-left">
 
-    </div>
-    <div class="uk-navbar-right">
+                <a class="uk-navbar-item uk-logo" href="#">Scrapbook</a>
 
-        <ul class="uk-navbar-nav">
-            <li>
-                <a href="#">Parent</a>
-                <div class="uk-navbar-dropdown">
-                    <ul class="uk-nav uk-navbar-dropdown-nav">
-                        <li class="uk-active"><a href="#">Active</a></li>
-                        <li><a href="#">Item</a></li>
-                        <li class="uk-nav-header">Header</li>
-                        <li><a href="#">Item</a></li>
-                        <li><a href="#">Item</a></li>
-                        <li class="uk-nav-divider"></li>
-                        <li><a href="#">Item</a></li>
-                    </ul>
+            </div>
+
+            <div class="nav-overlay uk-navbar-right">
+
+                <ul class="uk-navbar-nav">
+                    <a class="uk-navbar-toggle" uk-search-icon='' uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="#"></a>
+                    <li><a href="/signin">Sign-in</a></li>
+                </ul>
+
+            </div>
+
+            <div class="nav-overlay uk-navbar-left uk-flex-1" hidden>
+
+                <div class="uk-navbar-item uk-width-expand">
+                    <form class="uk-search uk-search-navbar uk-width-1-1">
+                        <input class="uk-search-input" type="search" placeholder="Search" autofocus />
+                    </form>
                 </div>
-            </li>
-        </ul>
-    </div>
-</nav>
+
+                <a class="uk-navbar-toggle" uk-close='' uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="#"></a>
+
+            </div>
+
+        </nav>
     )
 }
 
